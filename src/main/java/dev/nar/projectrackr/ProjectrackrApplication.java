160b3c6eed6a3a42863366e8ad8be88d58b09dd2
nassim <feddouli.nassim123@gmail.com>
2023-06-16 11:54:10 +0100

    ajout de fichiers

---
+++ src/main/java/dev/nar/projectrackr/ProjectrackrApplication.java
@@ -0,0 +1,13 @@
+package dev.nar.projectrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nar/projectrackr/ProjectrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nar.projectrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
