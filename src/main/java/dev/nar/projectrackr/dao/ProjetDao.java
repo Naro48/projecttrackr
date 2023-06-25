d0b702863873b52cce1ced67f6760cd405996c5a
nassim <feddouli.nassim123@gmail.com>
2023-06-25 17:11:36 +0100

    new commit

---
+++ src/main/java/dev/nar/projectrackr/dao/ProjetDao.java
@@ -0,0 +1,9 @@
+package dev.nar.projectrackr.dao;
+
+import dev.nar.projectrackr.entities.ProjetEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetDao extends JpaRepository<ProjetEntity,Integer> {
+
+
+}
